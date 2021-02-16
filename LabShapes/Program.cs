7ec57da528abfd5558f9756aa82d64f44768a89a
Andreas Nilsson <andreas.j.nilsson@outlook.com>
2021-02-16 09:11:48 +0100

    Add project files.

---
+++ LabShapes/Program.cs
@@ -0,0 +1,26 @@
+﻿using Shapes;
+using System;
+using System.Collections.Generic;
+
+namespace LabShapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shapes = new List<Shape>();
+            for (int i = 0; i < 20; i++)
+            {
+                shapes.Add(Shape.GenerateShape());
+            }
+
+            foreach (Shape s in shapes)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.Write("\nPress any key to continue . . .");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Shapes/Cuboid.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Cuboid : Shape3D
+    {
+        public Vector3 center;
+        public Vector3 size;
+
+        public Cuboid(Vector3 center, Vector3 size)
+        {
+            this.center = center;
+            this.size = size;
+        }
+
+        public Cuboid(Vector3 center, float width)
+        {
+            throw new NotImplementedException();
+        }
+        public override float Volume => throw new NotImplementedException();
+
+        public override Vector3 Center => throw new NotImplementedException();
+
+        public override float Area => throw new NotImplementedException();
+
+        public bool IsCube => throw new NotImplementedException();
+
+        public override string ToString() => throw new NotImplementedException();
+
+    }
+}
